12a3917526a2989e611d5134ac1f47e8b1c7631e
Peter Moldenhauer <molde003@gmail.com>
2016-12-19 23:13:47 -0600

    Added adtnl JavaScript fun files

---
+++ External JavaScript File/externalFileJS.js
@@ -0,0 +1,1 @@
+document.write("I am now using an external JavaScript file!"); 
---
+++ Functions 2/jsCode.js
@@ -0,0 +1,7 @@
+function addSomething(num, str){  // define a function with two parameters
+	var add = num + str;          // declare and initialize a variable to store the sum of the two parameters
+	
+	alert(add);                  // display an alert box containing the add variable 
+}
+
+addSomething(455, " programs to write.");  // call the function to use it 
